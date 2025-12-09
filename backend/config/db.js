013183a31c8f0f857b51d0d3a20fc9498aabd52a
AlthafMhdh <Alf11althaf@gmail.com>
2025-12-10 00:13:52 +0530

    update backend of db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -17,7 +17,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {});
+    //await mongoose.connect(process.env.MONGODB_URL, {});
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("DB Connected");
   } catch (err) {
     console.error("Error connecting DB", err);
