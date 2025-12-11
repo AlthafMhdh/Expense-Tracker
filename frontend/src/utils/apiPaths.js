604385f0c3710b3ac4450655c6120eb1158a67fa
AlthafMhdh <Alf11althaf@gmail.com>
2025-12-11 12:43:30 +0530

    vercel update

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -3,7 +3,7 @@ export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const API_PATHS = {
     AUTH : {
-        LOGIN : "/api/v1/auth/login",
+        LOGIN : "/auth/login",
         REGISTER : "/api/v1/auth/register",
         GET_USER_INFO : "/api/v1/auth/getUser",
     },
--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 import { BASE_URL } from "./apiPaths";
 
+// const axiosInstance = axios.create({
+//     baseURL : BASE_URL,
+//     timeout : 10000,
+//     headers : {
+//         "Content-Type" : "application/json",
+//         Accept : "application/json",
+//     },
+// });
+
 const axiosInstance = axios.create({
-    baseURL : BASE_URL,
-    timeout : 10000,
-    headers : {
-        "Content-Type" : "application/json",
-        Accept : "application/json",
-    },
+  baseURL: "https://expense-trackify-server.vercel.app/api/v1",
+  withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
