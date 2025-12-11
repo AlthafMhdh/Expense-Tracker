bfc290cc3e3db63764734eb503320df9105d64d5
AlthafMhdh <Alf11althaf@gmail.com>
2025-12-11 13:54:25 +0530

    final testing

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -134,15 +134,26 @@ export const loginUser = async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
-    if (!user || !(await user.comparePassword(password))) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
+    // if (!user || !(await user.comparePassword(password))) {
+    //   return res.status(400).json({ message: "Invalid credentials" });
+    // }
 
-    res.status(201).json({
-      id: user._id,
-      user,
-      token: generateToken(user._id),
-    });
+    // res.status(201).json({
+    //   id: user._id,
+    //   user,
+    //   token: generateToken(user._id),
+    // });
+
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    // generate JWT
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+    res.status(200).json({ user, token });
+    
   } catch (err) {
     res.status(500).json({
       message: "Error login user",
